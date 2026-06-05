f3c8cc4eb3c13be13bff67d4f13ec27bca19d6ab
dragosbajenaru1001 <155844538+dragosbajenaru1001@users.noreply.github.com>
2026-06-05 10:38:50 +0300

    Added InitialData

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -17,6 +17,9 @@ builder.Services.AddMarten(opts =>
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
+if(builder.Environment.IsDevelopment())
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
+
 builder.Services.AddValidatorsFromAssembly(assembly);
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
