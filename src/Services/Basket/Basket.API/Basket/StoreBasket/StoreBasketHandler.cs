5643d28e11fad30a83c6df4c4840e8e27988007d
dragosbajenaru1001 <155844538+dragosbajenaru1001@users.noreply.github.com>
2026-06-08 10:30:09 +0300

    Started implementeing Basket.APi microservice

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Basket.API.Basket.StoreBasket
+{
+    public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+
+    public record StoreBasketResult(string UserName);
+
+    public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+    {
+        public StoreBasketCommandValidator()
+        {
+            RuleFor(x => x.Cart).NotNull().WithMessage("Shopping cart cannot be null.");
+            RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("User name cannot be empty.");
+        }
+    }
+
+    public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+    {
+        public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+        {
+            var cart = command.Cart;
+            //TODO:Store basket in database
+            //ToDo:Update cache
+            return new StoreBasketResult("smn");
+        }
+    }
+}
