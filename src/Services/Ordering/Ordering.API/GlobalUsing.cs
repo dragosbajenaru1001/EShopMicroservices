5544f660ff3115766cd73601c435d18b96687141
dragosbajenaru1001 <155844538+dragosbajenaru1001@users.noreply.github.com>
2026-06-16 12:06:39 +0300

    Implemented Order.APi endpoints

---
+++ src/Services/Ordering/Ordering.API/GlobalUsing.cs
@@ -0,0 +1,13 @@
+﻿global using Carter;
+global using Mapster;
+global using MediatR;
+global using Ordering.Application.Dtos;
+global using Ordering.Application.Orders.Commands.CreateOrder;
+global using Ordering.Application.Orders.Commands.UpdateOrder;
+global using Ordering.Application.Orders.Commands.DeleteOrder;
+global using Ordering.Application.Orders.Queries.GetOrdersByName;
+global using Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+global using Ordering.API;
+global using Ordering.Application;
+global using Ordering.Infrastructure;
+global using Ordering.Infrastructure.Data.Extensions;
