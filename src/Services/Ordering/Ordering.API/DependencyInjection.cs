c3ff99dd5b5b52b7a247659a998d1b602cb66143
dragosbajenaru1001 <155844538+dragosbajenaru1001@users.noreply.github.com>
2026-06-17 11:01:46 +0300

    Added healthchecks and excepton handler

--- src/Services/Ordering/Ordering.API/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -2,9 +2,13 @@
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddCarter();
+
+            services.AddExceptionHandler<CustomExceptionHandler>();
+            services.AddHealthChecks()
+                .AddSqlServer(configuration.GetConnectionString("Database")!);
 
             return services;
         }
@@ -12,6 +16,13 @@
         public static WebApplication UseApiServices(this WebApplication app)
         {
             app.MapCarter();
+
+            app.UseExceptionHandler(options => { });
+            app.UseHealthChecks("/health",
+                new HealthCheckOptions
+                {
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
 
             return app;
         }
