f6f5009095dd1d02be33f7da1f2cb37cad0ec54b
dragosbajenaru1001 <155844538+dragosbajenaru1001@users.noreply.github.com>
2026-06-18 10:32:19 +0300

    Implemented feature management for OrderFulfilment

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.FeatureManagement;
 
 namespace Ordering.Application
 {
@@ -13,6 +14,8 @@ namespace Ordering.Application
                 cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
                 cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
             });
+
+            services.AddFeatureManagement();
 
             services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
 
