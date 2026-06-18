c1c9633900a1a3500c8fa94d7ec436b33c57f063
dragosbajenaru1001 <155844538+dragosbajenaru1001@users.noreply.github.com>
2026-06-18 10:24:16 +0300

    Created Ordering domain events and bascket checkout integration event

---
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using MassTransit;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain
+{
+    public class OrderCreatedEventHandler
+    (IPublishEndpoint publishEndpoint, ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
+    {
+        public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Domain Event handled: {DomainEvent}", domainEvent.GetType().Name);
+
+            var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+            await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        }
+    }
+}
